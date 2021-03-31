64af238560bfe3ac3706f29551d81adcf1f1ba8a
hejf <hejf@guoguokeji.com.cn>
2021-03-31 14:52:24 +0800

    feat: ui库搭建

---
+++ README.md
@@ -0,0 +1,8 @@
+# seen-ui
+所见即所得的ui库
+
+### setup
+```
+yarn
+yarn dev
+```
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+  setup() {}
+}
+</script>
