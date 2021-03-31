680cc7179191a1b4ac74af2ec7aa0de22885810e
hejf <hejf@guoguokeji.com.cn>
2021-03-31 16:43:11 +0800

    feat: 升级

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,11 @@ module.exports = {
       jsx: true
     }
   },
-  env: {
-    node: true,
-    es6: true,
-    browser: true
-  },
+
   extends: [
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'eslint:recommended'
+    'plugin:prettier/recommended'
   ],
   rules: {
     '@typescript-eslint/ban-ts-ignore': 'off',
@@ -25,6 +20,8 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': 'off',
@@ -45,14 +42,27 @@ module.exports = {
       }
     ],
     'space-before-function-paren': 'off',
-    quotes: ['error', 'single'],
-    'comma-dangle': ['error', 'never'],
-    'vue/require-default-prop': 'off',
-    'vue/custom-event-name-casing': 'off',
-    'no-use-before-define': 'off',
-    'vue/comment-directive': 'off',
+
+    'vue/attributes-order': 'off',
+    'vue/one-component-per-file': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
     'vue/singleline-html-element-content-newline': 'off',
-    'vue/html-self-closing': 'off',
-    'vue/max-attributes-per-line': 'off'
+    'vue/attribute-hyphenation': 'off',
+    // 'vue/html-self-closing': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
   }
 }
